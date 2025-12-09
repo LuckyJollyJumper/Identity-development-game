8c6a9d665c4189d25cbc7b18574cb05684ad4d35
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2025-12-09 22:33:03 +0100

    Small changes PersonaGame

--- Identity-development-game/Assets/Project/Scripts/SelectedItem.cs
+++ Identity-development-game/Assets/Project/Scripts/SelectedItem.cs
@@ -6,9 +6,7 @@ public class SelectedItem : MonoBehaviour, IPointerClickHandler
     private bool isSelected = false;
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Clicked on " + gameObject.name);
         isSelected = !isSelected;
-        // You can add visual feedback for selection here
         this.GetComponent<UnityEngine.UI.RawImage>().color = isSelected ? Color.green : Color.white;
     }
     
