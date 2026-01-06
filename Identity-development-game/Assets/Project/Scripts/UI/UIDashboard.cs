d49a74129c95f039ff353dd759b8da77bdb93b6b
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-01-06 12:35:24 +0100

    Add DashboardVisualiser scene and update UI assets
    Added the new DashboardVisualiser Unity scene and supporting UI script UIDashboard.cs. Updated LockerDouble prefab UI structure, replaced BuyCreditsBg.png with new sprites, and added several new sprite assets and their metadata for improved UI visuals.

---
+++ Identity-development-game/Assets/Project/Scripts/UI/UIDashboard.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class UIDashboard : MonoBehaviour
+{
+    [SerializeField] private GameObject ClassesPanel;
+    [SerializeField] private GameObject SettingsPanel;
+    [SerializeField] private GameObject StudentsPanel;
+    [SerializeField] private GameObject MainPanel;
+    public void OpenClassesPanel()
+    {
+        SettingsPanel.SetActive(false);
+        StudentsPanel.SetActive(false);
+        ClassesPanel.SetActive(true);
+    }
+
+    public void OpenSettingsPanel()
+    {
+        ClassesPanel.SetActive(false);
+        StudentsPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+    
+    public void OpenStudentsPanel()
+    {
+        ClassesPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        StudentsPanel.SetActive(true);
+    }
+    
+}
