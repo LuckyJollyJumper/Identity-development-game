c7a5e62396ff44fc7dd9f541197e605035a37be6
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-02-09 20:00:21 +0100

    Small bugfix in Inventory

--- Identity-development-game/Assets/Project/Scripts/UI/UIInventory.cs
+++ Identity-development-game/Assets/Project/Scripts/UI/UIInventory.cs
@@ -8,21 +8,28 @@ public class UIInventory : MonoBehaviour
 {
     [SerializeField] private GameObject ContentGrid; //Inventory item grid
     private List<ItemData> InventoryList; // Reference to all items in the players inventory
+    [Header("Debug")]
+    [SerializeField] private bool DebugMode = false;
+    private string DebugID = "[UI Inventory]";
     void Start(){
         InventoryList = GameManager.Instance._playerData.Inventory;
-        ReloadInventory();
     }
 
     /// <summary>
-    /// Reloads the inventory contents by looking at the players inventory in GameManager
+    /// Reloads the inventory contents by looking at the players inventory in GameManager. Is also called from the UIMainProfile
     /// </summary>
     public void ReloadInventory(){
+        foreach (Transform child in ContentGrid.transform){
+            Destroy(child.gameObject);
+        }
+
         this.InventoryList = GameManager.Instance._playerData.Inventory;
 
         foreach (ItemData shopItem in InventoryList){
             GameObject shopItemPrefab = Resources.Load<GameObject>("InventorySlot");
             shopItemPrefab.GetComponent<InventorySlot>().SetInventorySlot(shopItem);
             Instantiate(shopItemPrefab, ContentGrid.transform);
+            if(DebugMode){Debug.Log($"{DebugID} Loaded {shopItem.ItemName} in inventory");}
         }
     }
 }
