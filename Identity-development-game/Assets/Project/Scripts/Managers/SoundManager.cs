1436aa14cca92e195e6d09dbf78454de9e523bd1
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-03-02 11:38:52 +0100

    Updated Soundmanager

--- Identity-development-game/Assets/Project/Scripts/Managers/SoundManager.cs
+++ Identity-development-game/Assets/Project/Scripts/Managers/SoundManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SoundManager : MonoBehaviour
 {
@@ -12,7 +13,21 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    [SerializeField] public AudioClip BookFlip; // Array of all the sound clips used in the game
+    [SerializeField] public List<AudioClip> Footsteps;
+    [SerializeField] public GameObject SoundObject; // Reference to the soundObject on the player to play sounds from
+
     public void PlaySound(AudioClip clip, Transform source){
         AudioSource.PlayClipAtPoint(clip, source.position);
+    }
+    public void PlaySound(AudioClip clip){
+        AudioSource.PlayClipAtPoint(clip, SoundObject.transform.position);
+    }
+
+    public void PlayBookFlipSound(){
+        PlaySound(BookFlip, SoundObject.transform);
+    }
+    public void PlayFootstepSound(){
+        PlaySound(Footsteps[Random.Range(0, Footsteps.Count)], SoundObject.transform);
     }
 }
