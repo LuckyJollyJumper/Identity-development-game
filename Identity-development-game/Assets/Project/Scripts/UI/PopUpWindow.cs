27cfae99c204e10ecb936c64fe354810ea5bcb75
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-03-03 21:05:20 +0100

    Start of choices in PopUpWindow

--- Identity-development-game/Assets/Project/Scripts/UI/PopUpWindow.cs
+++ Identity-development-game/Assets/Project/Scripts/UI/PopUpWindow.cs
@@ -15,6 +15,7 @@ public class PopUpWindow : MonoBehaviour, IPointerClickHandler
     [Tooltip("List of texts to display in the pop-up window, in order")]
     [TextArea][SerializeField] public List<string> PopUpTexts;
     [SerializeField] public List<AudioClip> PopUpAudioClips; // Optional audio clips for each pop-up text
+    [SerializeField] public List<ChoiceField> PopUpChoiceOptions;
     [Tooltip("If true, the pop-up delete itself after all texts are displayed.")]
     [SerializeField] public bool DeleteOnClose = true; // If true, the pop-up GameObject will be destroyed when the interaction ends. Otherwise, it will just be deactivated.
     [Header("Debug")]
