79524340b181cdc099d085cc4ef27e4a30b7ff9e
Luc <91373173+LuckyJollyJumper@users.noreply.github.com>
2026-03-03 10:42:09 +0100

    Start of UI manager

---
+++ Identity-development-game/Assets/Project/Scripts/Managers/UIManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject MainUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
